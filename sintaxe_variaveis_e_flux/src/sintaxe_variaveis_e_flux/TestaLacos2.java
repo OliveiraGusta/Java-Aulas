7967be550bc688a9dcb95b6bbea457c756b142db
OliveiraGusta <gustavo.oliver456@gmail.com>
2023-03-22 17:45:01 -0300

    feat(Lacos de repeticao): While e For

---
+++ sintaxe_variaveis_e_flux/src/sintaxe_variaveis_e_flux/TestaLacos2.java
@@ -0,0 +1,13 @@
+package sintaxe_variaveis_e_flux;
+
+public class TestaLacos2 {
+	public static void main(String[] args) {
+		for (int linha = 1; linha < 10; linha++) {
+			for (int coluna = 0; coluna == linha; coluna++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
