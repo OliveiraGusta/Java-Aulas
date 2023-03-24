534a7b473f565026a588a5c6a89c8dcaa1010821
OliveiraGusta <gustavo.oliver456@gmail.com>
2023-03-24 16:13:09 -0300

    feat(Metodos): Metodos com retorno, metodos dentro de metodos

--- econo_me_bank/src/econo_me_bank/Conta.java
+++ econo_me_bank/src/econo_me_bank/Conta.java
@@ -2,7 +2,29 @@ package econo_me_bank;
 
 public class Conta {
 	double saldo;
-	int agencia = 22;
+	int agencia;
 	int numero;
 	String titular;
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		return false;
+
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saca(valor);
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
 }
---
+++ econo_me_bank/src/econo_me_bank/TestaMetodo.java
@@ -0,0 +1,35 @@
+package econo_me_bank;
+
+public class TestaMetodo {
+	public static void main(String[] args) {
+		Conta contaDoGustavo = new Conta();
+		contaDoGustavo.saldo = 100;
+		contaDoGustavo.deposita(50);
+		
+		System.out.println(contaDoGustavo.saldo);
+		
+		if(contaDoGustavo.saca(20)) {
+			System.out.println("Saque realizado com sucesso!" );
+			System.out.println("Seu saldo atual é de: R$" + contaDoGustavo.saldo);
+		}else {
+			System.out.println("Ocorreu um erro ao realizar o saque de: R$" + contaDoGustavo.saldo);
+			System.out.println("Você não tem esse valor na conta");
+		}
+		
+		Conta contaDaNivea = new Conta();
+		contaDaNivea.deposita(1000);
+		
+	
+		if(contaDaNivea.transfere(3000, contaDoGustavo)) {
+			System.out.println("Transferencia realizada com sucesso");
+			System.out.println("Seu saldo atual R$" + contaDaNivea.saldo);
+		}else {
+			System.out.println("Você não tem o saldo insuficente para a transferencia");
+			System.out.println("Seu saldo atual é de R$" + contaDaNivea.saldo);
+		}
+		
+		System.out.println(contaDaNivea.saldo);
+		System.out.println(contaDoGustavo.saldo);
+
+	}
+}
